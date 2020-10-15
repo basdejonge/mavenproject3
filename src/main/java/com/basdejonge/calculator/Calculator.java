69c1ac1719abf3c9dbd75841205a4109470f19fc
Bas de Jonge <bas.de.jonge@heroes-corp.com>
2020-10-15 11:03:45 +0200

    Update Calculator.java

--- src/main/java/com/basdejonge/calculator/Calculator.java
+++ src/main/java/com/basdejonge/calculator/Calculator.java
@@ -4,14 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
-@SpringBootApplication
-public class Calculator {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
-
-}
 @Service
 public class Calculator {
 int sum(int a, int b) {
