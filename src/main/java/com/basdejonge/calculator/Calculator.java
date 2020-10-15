f2f303b59e7932f529cd2b5d68f882b471c7474f
Bas de Jonge <bas.de.jonge@heroes-corp.com>
2020-10-15 10:58:39 +0200

    Update Calculator.java

--- src/main/java/com/basdejonge/calculator/Calculator.java
+++ src/main/java/com/basdejonge/calculator/Calculator.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class Calculator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
